907e10d6561bac1ccbfb48c4885b1b49590622dc
MrRainbowYoo <813806051@qq.com>
2022-02-21 22:21:16 +0800

    good night

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
+"use strict";
 // let hd = "houdunren.com"
 // console.log(hd)
 // const arr = ['houdunren.com',1,false]
@@ -29,6 +30,66 @@
 // let hd1:unknown = '后盾人'
 // // let test1:string = hd1 // 会报错，修改为下面这一行
 // let test1:string = hd1 as string // 类型断言
-var str = '99';
-var num = str; // unknown作为中间商
-console.log(num);
+// let str: string = '99'
+// let num: number = str as unknown as number // unknown作为中间商
+// console.log(typeof(num)) // 这里还是string ？？
+// let num1:number = 11
+// let str1:string = num1 as unknown as string
+// console.log(typeof(str1)) // 这里也还是number ？？  （只是可以跑通 实际上没什么意义）
+// let xx:void = undefined  // void类型 undefined和null
+// xx = null
+// xx = '111' // 不允许
+// function hh():void{}
+// hh() // 返回undefined
+// never不会返回任何东西，适用于抛出异常
+// function hh():never{ 
+//     throw new Error('类型错误')
+// }
+// hh()
+// function sum(a, b:number, c?:string){
+//     // ?: 表示可选 若没有 则默认为undefined
+//     // c:string = '666' 若通过这种方式设置了默认值，则不需要:? 可选参数
+//     c = c || '666'
+//     console.log(a,b,c)
+// }
+// sum(1,2)
+// let addUser = (user:{name:string,age:number}):void => {
+//     console.log('添加用户')
+// }
+// let updateUser = (user:{name:string,age:number}):void => {
+//     console.log('更新用户')
+// }
+// addUser({name:'ccc',age:11})
+// updateUser({name:'ddd',age:18})
+// **上述这番操作，两个函数的形参结构是一致的，如果需要修改，那么工作量会很大，因此可以使用 type 关键词，改为下面的写法
+// type userType = {name:string; age:number} // 注意：用的是分号，但我之前写了逗号也没报错
+// let addUser1 = (user:userType):void => {
+//     console.log('添加用户')
+// }
+// let updateUser1 = (user:userType):void => {
+//     console.log('更新用户')
+// }
+// addUser1({name:'aa',age:18})
+// updateUser1({name:'bb',age:1})
+// 函数的结构定义
+// let hd:(a:number,b:number) => number // 函数的结构定义
+// hd = (x:number,y:number):number => { // 函数的具体实现
+//     return(x+y)
+// }
+// console.log(hd(1,1))
+// type userType = {name:string; age:number}
+// type userFunType = (user:userType) => boolean
+// let addUser:userFunType = (user:userType):boolean => {
+//     return true
+// }
+// addUser({name:'11',age:1})
+// 剩余参数
+// function sum(...args:number[]):number {
+//     return args.reduce((pre,cur)=>pre+cur,0)
+// }
+// console.log(sum(1,2,3,4))
+// 元组 规定每个位置的类型 => 可以改变值，不能改变类型
+// let turple:[string, number, boolean] = ['111',2,true]
+// turple[0] = true // 会报错
+// turple[0] = 'xxxxx' // 不会报错
+// console.log(turple)
---
+++ 2.js
@@ -0,0 +1,42 @@
+"use strict";
+// enum SexType{
+//     BOY = 6,
+//     GIRL
+// }
+// console.log(SexType.BOY)
+// 断言 as
+// function fun(args:boolean):(string | number) {
+//     return args ? '我就干' : 2022
+// }
+// let res = fun(true) as number
+// res = '1111' // 会报错
+// console.log(res)
+// as const
+// let a = 'abc' // string 类型
+// let b:'abc' // 值类型
+// // b = '1' // 会报错
+// let c = 'abc' as const // 与 let c:'abc' = 'abc' 相同
+// let a:string = 'helloworld'
+// let b = 123
+// let arr = [a, b] as const // arr 变成元组 arr[0]为string类型，arr[1]为数值类型
+// let arr1 = [a, b, 1, true, '123'] as const // 除了a和b，其余位置都是值类型，即只能是1，true和'123'
+// let c = 99 as const
+// let arr2 = [a, c] as const // a是string类型，c是值类型， 只能是99
+// **** as const 根据具体的值来提取类型
+// function fun(){
+//     let a = 'hello world'
+//     let b = (x:number, y:number):number => x + y
+//     return [a,b]
+// }
+// const [n, m] = fun();
+// m(1,2) // 会报错 没法调用 因为m的类型要么是string要么是Function，不明确
+// console.log((m as Function)(1,2)) // 可以调用，在调用前通过断言将m明确为Function类型，当然也可以不写Function 具体写 m as (x:number,y:number)=>number
+// 下面这样也是可以的
+// const [n, m] = fun() as [number, (x:number, y:number) => number]
+// console.log(m(1,2))
+// 在函数返回的时候也可以操作
+// return [a, b] as [number, (x:number, y:number) => number] 或者 return [a, b] as [typeof(a), typeof(b)]
+// 简化一下 return [a, b] as const  
+// 非空断言 ! 明确告诉是有值的
+// const el:HTMLDivElement = document.querySelector('.hello')!
+// console.log(el.innerHTML)
